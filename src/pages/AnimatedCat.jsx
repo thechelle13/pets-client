04b8e853195f4a4094c44394ddacb14dcfbaa774
Michelle Totherow <tym4me13@gmail.com>
2024-05-25 21:18:46 -0500

    creation of animated cat features

---
+++ src/pages/AnimatedCat.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import './AnimatedCat.css';
+
+export const AnimatedCat = () => {
+  return (
+    <div className="cat-container">
+      <div className="cat">
+        <div className="cat-head">
+          <div className="cat-ears">
+            <div className="cat-left-ear">
+              <div className="cat-left-inner-ear"></div>
+            </div>
+            <div className="cat-right-ear">
+              <div className="cat-right-inner-ear"></div>
+            </div>
+          </div>
+          <div className="cat-eyes">
+            <div className="cat-left-eye">
+              <div className="cat-left-inner-eye"></div>
+            </div>
+            <div className="cat-right-eye">
+              <div className="cat-right-inner-eye"></div>
+            </div>
+          </div>
+          <div className="cat-nose"></div>
+          <div className="cat-mouth">
+            <div className="cat-mouth-line-left"></div>
+            <div className="cat-mouth-line-right"></div>
+          </div>
+          <div className="cat-whiskers">
+            <div className="cat-whiskers-left">
+              <div className="cat-whisker-left-top"></div>
+              <div className="cat-whisker-left-middle"></div>
+              <div className="cat-whisker-left-bottom"></div>
+            </div>
+            <div className="cat-whiskers-right">
+              <div className="cat-whisker-right-top"></div>
+              <div className="cat-whisker-right-middle"></div>
+              <div className="cat-whisker-right-bottom"></div>
+            </div>
+          </div>
+        </div>
+        <div className="cat-body"></div>
+        <div className="cat-tail"></div>
+      </div>
+    </div>
+  );
+};
+
+
