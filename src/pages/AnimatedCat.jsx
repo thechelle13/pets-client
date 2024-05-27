5e6abbe4d2e6fcc89ad8c29b2459eac99c143eae
Michelle Totherow <tym4me13@gmail.com>
2024-05-27 12:55:03 -0500

    clean up

--- src/pages/AnimatedCat.jsx
+++ src/pages/AnimatedCat.jsx
@@ -4,42 +4,42 @@ import './AnimatedCat.css';
 export const AnimatedCat = () => {
   
   return (
-  <div class="cat-head">
-  <div class="cat-ears">
-    <div class="cat-left-ear">
-      <div class="cat-left-inner-ear"></div>
+  <div className="cat-head">
+  <div className="cat-ears">
+    <div className="cat-left-ear">
+      <div className="cat-left-inner-ear"></div>
     </div>
-    <div class="cat-right-ear">
-      <div class="cat-right-inner-ear"></div>
+    <div className="cat-right-ear">
+      <div className="cat-right-inner-ear"></div>
     </div>
   </div>
 
-  <div class="cat-eyes">
-    <div class="cat-left-eye">
-      <div class="cat-left-inner-eye"></div>
+  <div className="cat-eyes">
+    <div className="cat-left-eye">
+      <div className="cat-left-inner-eye"></div>
     </div>
-    <div class="cat-right-eye">
-      <div class="cat-right-inner-eye"></div>
+    <div className="cat-right-eye">
+      <div className="cat-right-inner-eye"></div>
     </div>
   </div>
   
-  <div class="cat-nose"></div>
+  <div className="cat-nose"></div>
 
-  <div class="cat-mouth">
-    <div class="cat-mouth-line-left"></div>
-    <div class="cat-mouth-line-right"></div>
+  <div className="cat-mouth">
+    <div className="cat-mouth-line-left"></div>
+    <div className="cat-mouth-line-right"></div>
   </div>
 
-  <div class="cat-whiskers">
-    <div class="cat-whiskers-left">
-      <div class="cat-whisker-left-top"></div>
-      <div class="cat-whisker-left-middle"></div>
-      <div class="cat-whisker-left-bottom"></div>
+  <div className="cat-whiskers">
+    <div className="cat-whiskers-left">
+      <div className="cat-whisker-left-top"></div>
+      <div className="cat-whisker-left-middle"></div>
+      <div className="cat-whisker-left-bottom"></div>
     </div>
-    <div class="cat-whiskers-right">
-      <div class="cat-whisker-right-top"></div>
-      <div class="cat-whisker-right-middle"></div>
-      <div class="cat-whisker-right-bottom"></div>
+    <div className="cat-whiskers-right">
+      <div className="cat-whisker-right-top"></div>
+      <div className="cat-whisker-right-middle"></div>
+      <div className="cat-whisker-right-bottom"></div>
     </div>
   </div>
   <div className="purring purring--1">Prr</div>
