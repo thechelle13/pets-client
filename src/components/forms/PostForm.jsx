77b3e5c3882dc760f221a91c62eaaf6a3cddee81
Michelle Totherow <tym4me13@gmail.com>
2024-06-08 11:42:08 -0500

    refactor for styling and change input of post description

--- src/components/forms/PostForm.jsx
+++ src/components/forms/PostForm.jsx
@@ -26,24 +26,24 @@ export const PostForm = () => {
   };
 
   return (
-    <main className="bg-gradient-to-b from-blue-500 to-purple-500 h-full">
-      <div className="bg-gradient-to-b from-green-200 to-green-800 text-center my-8 p-6 rounded-lg shadow-lg max-w-md mx-auto">
+    <main className="bg-gradient-to-b from-blue-500 to-purple-500 min-h-screen flex flex-col">
+    <div className="flex flex-col items-center mt-4">
+      <form onSubmit={handleSubmit} className="bg-gradient-to-b from-green-200 to-green-800 text-center p-6 rounded-lg shadow-lg w-full max-w-2xl mx-auto">
         <h1 className="text-5xl font-semibold mb-4 text-white">Add Post</h1>
-
-        <form onSubmit={handleSubmit}>
-          <div className="form-field mb-4">
-            <label className="block font-bold mb-1 text-black" htmlFor="description">
-              Description:
-            </label>
-            <input
-              className="textarea-field border p-2 w-full"
-              type="text"
-              name="description"
-              id="description"
-              value={postData.description}
-              onChange={handleInputChange}
-            />
-          </div>
+        <div className="form-field mb-4">
+          <label className="block font-bold mb-1 text-black" htmlFor="description">
+            Description:
+          </label>
+          <textarea
+            className="textarea-field border border-gray-300 rounded-md p-2 w-full h-32 resize-none"
+            name="description"
+            id="description"
+            value={postData.description}
+            onChange={handleInputChange}
+            maxLength={200}
+            placeholder="Enter your post here (max 200 characters)"
+          />
+        </div>
           <div className="form-field mb-4">
             <label className="block font-bold mb-1 text-black" htmlFor="sitStartDate">
               Sit Start Date:
