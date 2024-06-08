f50f75c7a4c6b861ba62d2423cadb28d6faebc9f
Michelle Totherow <tym4me13@gmail.com>
2024-06-08 11:31:37 -0500

    Cont work on pet creation in UI, styling updates

--- src/components/forms/PetForm.jsx
+++ src/components/forms/PetForm.jsx
@@ -15,11 +15,14 @@ export const PetForm = ({ token }) => {
   const [types, setTypes] = useState([]);
   const [loading, setLoading] = useState(true);
 
+  const navigate = useNavigate();
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         if (token) {
           const userData = await getUser(token);
+          console.log("User Data:", userData);
           setUser(userData);
 
           const typesData = await getTypes();
@@ -37,7 +40,6 @@ export const PetForm = ({ token }) => {
     fetchData();
   }, [token]);
 
-  const navigate = useNavigate();
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -48,25 +50,16 @@ export const PetForm = ({ token }) => {
     navigate("/");
   };
 
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   console.log("Pet data submitted:", petData);
-  // };
-
-  // if (loading) {
-  //   console.log("Loading...");
-  //   return <div>Loading...</div>;
-  // }
-  // console.log("Rendering form...");
-
-
   const handleSubmit = async (e) => {
     e.preventDefault();
+    console.log("Submitting pet data:", petData);
+    console.log("User ID:", user.id); 
     try {
-      const response = await updatePet(user.id, petData); 
+      console.log("User ID:", user.id);
+      const response = await updatePet(user.id, petData);
+      console.log("Update pet response:", response); 
       if (response.status === 200) {
         console.log("Pet data submitted successfully:", petData);
-      
         navigate("/");
       } else {
         console.error("Failed to submit pet data:", response.statusText);
@@ -76,90 +69,82 @@ export const PetForm = ({ token }) => {
     }
   };
   
-  
-
+    
   return (
-    <div className="mx-auto max-w-md">
-      <h1 className="text-xl font-bold mb-4">Add Pet</h1>
-      <form onSubmit={handleSubmit} className="space-y-4">
-        <div className="flex items-center">
-          <label htmlFor="name" className="mr-2">Pet Name:</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={petData.name}
-            onChange={handleInputChange}
-            className="border border-gray-300 rounded-md px-3 py-2 w-full"
-            autoComplete="name"
-          />
-        </div>
+    <main className="bg-gradient-to-b from-blue-500 to-purple-500 min-h-screen flex flex-col">
+      <div className="flex flex-col items-center mt-4">
+        <form onSubmit={handleSubmit} className="bg-gradient-to-b from-green-200 to-green-800 text-center p-4 rounded-lg shadow-lg max-w-md mx-auto">
+          <h1 className="text-5xl font-semibold mb-4 text-white">Add Pet</h1>
+          <div className="flex items-center mb-4">
+            <label htmlFor="name" className="mr-2">Pet Name:</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={petData.name}
+              onChange={handleInputChange}
+              className="border border-gray-300 rounded-md px-3 py-2 w-full"
+              autoComplete="name"
+            />
+          </div>
 
-        <div className="flex items-center">
-          <label htmlFor="type" className="mr-2">Type:</label>
-          <select
-            name="type"
-            id="type"
-            value={petData.type}
-            onChange={handleInputChange}
-            className="border border-gray-300 rounded-md px-3 py-2 w-full"
-            style={{ color: 'black', backgroundColor: 'white' }}
-          >
-            <option value="">Select type</option>
-            {types.map((type) => {
-             
-              return (
+          <div className="flex items-center mb-4">
+            <label htmlFor="type" className="mr-2">Type:</label>
+            <select
+              name="type"
+              id="type"
+              value={petData.type}
+              onChange={handleInputChange}
+              className="border border-gray-300 rounded-md px-3 py-2 w-full"
+              style={{ color: 'black', backgroundColor: 'white' }}
+            >
+              <option value="">Select type</option>
+              {types.map((type) => (
                 <option 
                   key={type.id} 
                   value={type.id} 
                   style={{ color: 'black', backgroundColor: 'white' }}
                 >
-
                   {type.label}
                 </option>
-              );
-            })}
-          </select>
-        </div>
+              ))}
+            </select>
+          </div>
 
-        <div className="flex items-center">
-          <label htmlFor="image_url" className="mr-2">Image URL:</label>
-          <input
-            type="text"
-            id="image_url"
-            name="image_url"
-            value={petData.image_url}
-            onChange={handleInputChange}
-            className="border border-gray-300 rounded-md px-3 py-2 w-full"
-            autoComplete="url"
-          />
-        </div>
+          <div className="flex items-center mb-4">
+            <label htmlFor="image_url" className="mr-2">Image URL:</label>
+            <input
+              type="text"
+              id="image_url"
+              name="image_url"
+              value={petData.image_url}
+              onChange={handleInputChange}
+              className="border border-gray-300 rounded-md px-3 py-2 w-full"
+              autoComplete="url"
+            />
+          </div>
 
-        
-        
-        <div className="flex justify-between mb-4">
-          <button
-            type="submit"
-            className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition-colors"
-            onClick={handleSubmit}
-          >
-            Add Pet
-          </button>
-          <button
-            type="button"
-            className="bg-gray-500 text-white px-4 py-2 rounded-md hover:bg-gray-600 transition-colors"
-            onClick={handleCancel}
-          >
-            Cancel
-          </button>
-        </div>
-      </form>
-    
-      <div  className="mt-16">
-  <SleepingCat />
-</div>
-
-    
-    </div>
+          <div className="flex justify-between mb-4">
+            <button
+              type="submit"
+              className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition-colors"
+              onClick={handleSubmit}
+            >
+              Add Pet
+            </button>
+            <button
+              type="button"
+              className="bg-gray-500 text-white px-4 py-2 rounded-md hover:bg-gray-600 transition-colors"
+              onClick={handleCancel}
+            >
+              Cancel
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className="mt-16">
+        <SleepingCat />
+      </div>
+    </main>
   );
 };
