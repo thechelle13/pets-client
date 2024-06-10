b00b5f6c7fa2fce2347706277507ae64c4e8672a
Michelle Totherow <tym4me13@gmail.com>
2024-06-10 10:00:18 -0500

    refactor to update tailwind styling, add more data to post

--- src/pages/PostList.jsx
+++ src/pages/PostList.jsx
@@ -1,12 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { addCommentToPost, addLikeToPost, getAllPosts } from "../services/postServices";
-
+import { getUser } from "../services/userServices";
 
 export const PostList = ({ setToken, token }) => {
-    // const [user, setUser] = useState(null);
+    const [user, setUser] = useState({});
     const [posts, setPosts] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                if (token) {
+                    const userData = await getUser();
+                    console.log("User Data:", userData);
+                    setUser(userData);
+                    setIsLoading(false); 
+                }
+            } catch (error) {
+                console.error("Error fetching data:", error);
+                setIsLoading(false); 
+            }
+        };
+        fetchData();
+    }, [token]);
+    
 
     useEffect(() => {
         getAllPosts().then(postsArray => {
@@ -15,51 +33,24 @@ export const PostList = ({ setToken, token }) => {
         });
     }, []);
 
-    // const handleAddComment = async (postId, commentText) => {
-    //     // Call the service function to add a comment to the post
-    //     await addCommentToPost(postId, commentText);
-    //     // Refresh posts after adding comment
-    //     getAllPosts().then(postsArray => {
-    //         setPosts(postsArray);
-    //     });
-    // };
-
-    // const handleAddLike = async (postId) => {
-    //     // Call the service function to add a like to the post
-    //     await addLikeToPost(postId);
-    //     // Refresh posts after adding like
-    //     getAllPosts().then(postsArray => {
-    //         setPosts(postsArray);
-    //     });
-    // };
-
     return (
-        <>
-         <main className="bg-gradient-to-b from-blue-500 to-purple-500 h-full">
-            <h1 className="text-3xl text-center mb-4">Post List</h1>
+        <main className="bg-gradient-to-b from-blue-500 to-purple-500 min-h-screen">
+            <h1 className="text-center text-5xl font-semibold mb-4 text-white">Post List</h1>
             {isLoading ? (
-                <h3>Loading Posts...</h3>
+                <h3 className="text-white text-center">Loading Posts...</h3>
             ) : (
-                <div>
+                <div className="mx-auto max-w-lg">
                     {posts.map(post => (
                         <div key={post.id} className="my-4 p-4 border rounded">
+                            <h3>PetUser #: {post.pet_user.id}</h3>
+                            {user && <h3>User Name: {user.username}</h3>}
+                            {/* <h3>User Name: {user.username}</h3> */}
                             <h2 className="text-lg font-semibold">{post.description}</h2>
                             <p>Start Date: {post.sitStartDate}</p>
                             <p>End Date: {post.sitEndDate}</p>
-                          
-                            {/* <p>Owner: {post.petuser.user.username}</p> */}
                             <p>City: {post.pet_user.city}</p>
-                            <div className="flex items-center justify-between">
-                            {/* <button 
-                                    className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-700 focus:outline-none focus:ring focus:border-blue-300 flex items-center space-x-1"
-                                    onClick={() => handleAddLike(post.id)}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 5v14m0-14l-4 4m4-4l4 4M5 12h14"/>
-                                    </svg>
-                                    <span>Like</span>
-                                </button> */}
-                                <Link key={post.id} to={`/postLists/${post.id}`} 
-                                className="text-blue-800">
+                            <div className="flex justify-between items-center mt-4">
+                                <Link to={`/postLists/${post.id}`} className="text-blue-800">
                                     View Details
                                 </Link>
                             </div>
@@ -67,7 +58,6 @@ export const PostList = ({ setToken, token }) => {
                     ))}
                 </div>
             )}
-            </main>
-        </>
+        </main>
     );
 };
